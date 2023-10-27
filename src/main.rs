9a3a5cb38c9e781dd601d1e1811f7526ac8a6553
DISTREAT <99132213+DISTREAT@users.noreply.github.com>
2023-10-27 02:28:06 +0200

    feat: basic command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{Parser, Subcommand};
+
+mod cli;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct ArgumentParser {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Install an asset from a GitHub repository
+    Install {
+        /// The repository to install from (ex. DISTREAT/projavu)
+        repository: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = ArgumentParser::parse();
+
+    match &arguments.command {
+        Commands::Install { repository } => cli::install::install_package(repository),
+    }
 }
